6c077196efbb884c81d7b47f9f8dc3cbba9ca971
hayden <haydenchuter@gmail.com>
2017-12-03 00:06:40 -0800

    Finishing up the second part, didnt realise this existed earlier

--- Day Two/src/main.cpp
+++ Day Two/src/main.cpp
@@ -20,7 +20,7 @@
 
 #include "main.h"
 
-#define ASCII_TO_NUM	48
+int part_two_solver(std::vector<int> input_vec);
 
 int main(int argc, char *argv[])
 {
@@ -37,6 +37,7 @@ int main(int argc, char *argv[])
 	int					min = 0;
 	int 				max = 0;
 	int					sum = 0;
+	int					sum_p2 = 0;
 
 	do
 	{
@@ -53,12 +54,47 @@ int main(int argc, char *argv[])
 			max = *std::max_element(input_line_int.begin(), input_line_int.end());
 			min = *std::min_element(input_line_int.begin(), input_line_int.end());
 			sum += std::abs(max - min);
+			sum_p2 += part_two_solver(input_line_int);
 		}
 
 	}while(ifs.eof() != true);
 
 
-	std::cout << sum;
+	std::cout << sum << "\n" << sum_p2;
 
     return 0;
 }
+
+int part_two_solver(std::vector<int> input_vec)
+{
+	int ret_val;
+
+	for(int i = 0; i < input_vec.size(); i++)
+	{
+		for(int j = 0; j < input_vec.size(); j++)
+		{
+			if(i == j)
+			{
+			}
+			else if(input_vec.at(i) >= input_vec.at(j))
+			{
+				if(input_vec.at(i) % input_vec.at(j) == 0)
+				{
+					return input_vec.at(i) / input_vec.at(j);
+				}
+			}
+			else if(input_vec.at(i) < input_vec.at(j))
+			{
+				if(input_vec.at(j) % input_vec.at(i) == 0)
+				{
+					return input_vec.at(j) / input_vec.at(i);
+
+				}
+			}
+		}
+	}
+}
+
+
+
+
