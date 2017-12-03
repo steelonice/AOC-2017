16443fc69b7776cdc9dd18f44971b40c375de90c
steelonice <haydenchuter@gmail.com>
2017-12-02 19:02:57 -0800

    Completing day three part two

--- Day Three/src/main.cpp
+++ Day Three/src/main.cpp
@@ -20,7 +20,9 @@
 
 #include "main.h"
 
-#define MY_POS	(347991-1 )
+#define MY_POS			( 347991-1 )
+#define ARRAY_CENTER 	512
+
 
 typedef struct{
 	int x_pos;
@@ -34,14 +36,24 @@ typedef enum{
 	RIGHT	= 0
 }direction;
 
+int calculate_position_value(int map[][1024], coord current_pos);
+
+int value[1024][1024];
+
+
 int main(int argc, char *argv[])
 {
 	int step_size = 1;
 	int	steps = 0;
 	int direction = 0;
+	bool	part_two_done = false;
 	coord position;
 
+	memset(value, '\0', sizeof(value));
+
 	position.x_pos = position.y_pos = 0;
+
+	value[ARRAY_CENTER][ARRAY_CENTER] = 1;
 
 	for(int loop = 0; loop < MY_POS; loop ++)
 	{
@@ -60,6 +72,7 @@ int main(int argc, char *argv[])
 				position.y_pos -= 1;
 				break;
 		}
+		value[position.x_pos + ARRAY_CENTER][position.y_pos + ARRAY_CENTER] = calculate_position_value(value, position);
 		steps ++;
 		if( steps % step_size == 0)
 		{
@@ -70,11 +83,35 @@ int main(int argc, char *argv[])
 		{
 			step_size ++;
 		}
+		if(value[position.x_pos + ARRAY_CENTER][position.y_pos + ARRAY_CENTER] > MY_POS && part_two_done == false)
+		{
+			part_two_done = true;
+			std::cout << "P2: Value: " << value[position.x_pos + ARRAY_CENTER][position.y_pos + ARRAY_CENTER]
+			<< "Y Pos: " << position.y_pos
+			<< " X Pos:" << position.x_pos << "\n";
+		}
 	}
 
-	std::cout << "Y Pos: " << position.y_pos
+	std::cout << "P1: Y Pos: " << position.y_pos
 			<< " X Pos:" << position.x_pos
 			<< " Total Moves: " << std::abs(position.x_pos) + std::abs(position.y_pos);
 
 	return 0;
 }
+
+int calculate_position_value(int map[][1024], coord current_pos)
+{
+	int sum = 0;
+	sum += map[current_pos.x_pos+1 + ARRAY_CENTER][current_pos.y_pos+1 + ARRAY_CENTER];
+	sum += map[current_pos.x_pos+1 + ARRAY_CENTER][current_pos.y_pos + ARRAY_CENTER];
+	sum += map[current_pos.x_pos+1 + ARRAY_CENTER][current_pos.y_pos-1 + ARRAY_CENTER];
+	sum += map[current_pos.x_pos-1 + ARRAY_CENTER][current_pos.y_pos+1 + ARRAY_CENTER];
+	sum += map[current_pos.x_pos-1 + ARRAY_CENTER][current_pos.y_pos + ARRAY_CENTER];
+	sum += map[current_pos.x_pos-1 + ARRAY_CENTER][current_pos.y_pos-1 + ARRAY_CENTER];
+	sum += map[current_pos.x_pos + ARRAY_CENTER][current_pos.y_pos+1 + ARRAY_CENTER];
+	sum += map[current_pos.x_pos + ARRAY_CENTER][current_pos.y_pos-1 + ARRAY_CENTER];
+	return sum;
+}
+
+
+
